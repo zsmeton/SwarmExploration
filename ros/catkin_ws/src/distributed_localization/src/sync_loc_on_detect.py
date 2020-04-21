4f2da504bee76b853cf63af95df1411dfb53c953
Josh Rands <jmrands@mymail.mines.edu>
2020-04-21 09:20:52 -0600

    Set up infrastructure for combining localization data with relative pose data.

--- ros/catkin_ws/src/distributed_localization/src/sync_loc_on_detect.py
+++ ros/catkin_ws/src/distributed_localization/src/sync_loc_on_detect.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # license removed for brevity
+
 # sync_loc_on_detect.py
 # Synchronize probabilistic locations of multiple robots using detection data
 
@@ -24,25 +25,54 @@ def sync_loc_data(robot_n, robot_m):
     Sync robot_n and robot_m's maps using detection data between them
     Note: This function should be called on new detection data 
     """
+    global current_loc_data
+    global current_relative_pose_data
+
+    print("Syncing localization data between robot " + robot_n + " and robot " + robot_m)
 
     # use global variables for current data 
     # TODO: SIMULATE SOME SORT OF COMMS HERE FOR "SHARING" OF DATA
+    robot_n_particlecloud = current_loc_data[robot_n]
+    robot_m_particlecloud = current_loc_data[robot_m]
+
+    if None == robot_n_particlecloud or None == robot_m_particlecloud:
+        rospy.logwarn("Cannot sync particlecloud data for " + robot_n + " and " + robot_m + ": Particle cloud is None")
+        return 
+
+    n_detect_m = current_relative_pose_data[robot_n][robot_m]
+    if None == n_detect_m:
+        rospy.logerr("Sync localization called but no relative pose data. This shouldn't happen.")
+
+    # TODO: Sync localization data! 
 
 
 def particlecloud_callback(particlecloud,robot_name):
     """
     Callback function for particlecloud topics
     """
-    print(particlecloud)
-    print(robot_name)
+    global current_loc_data
+
+    # update the global current location particle data of this robot 
+    current_loc_data[robot_name] = particlecloud.poses
 
 
 def relative_pose_callback(relative_pose,bot_names_tuple):
     """
     Callback function for relative pose topics 
     """
-    print(relative_pose)
+    global current_relative_pose_data
+
     print("Robot " + bot_names_tuple[0] + " observed robot " + bot_names_tuple[1])
+
+    # parse bot names out of tuple argument
+    observing_bot = bot_names_tuple[0]
+    observed_bot = bot_names_tuple[1]
+
+    # update global map
+    current_relative_pose_data[observing_bot][observed_bot] = relative_pose 
+
+    # sync localization data of these bots because they detected each other
+    sync_loc_data(observing_bot, observed_bot)
 
 
 def run_sync(num_bots):
@@ -76,6 +106,7 @@ def run_sync(num_bots):
 
     while not rospy.is_shutdown():
         rate.sleep()
+
 
 if __name__ == '__main__':
     try:
